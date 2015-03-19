ec71648bd95ae6f9b1d12458e14cf035f0f20229
Diego Spiegelberg Zuge <diegoszch@gmail.com>
2015-03-18 21:06:14 -0300

    Commit inicial

---
+++ Certificacao70-480.Capitulo2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Certificacao70_480.Capitulo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Executar(new ExemploSwitchCase());
+
+
+            Console.ReadKey();
+        }
+
+        public static void Executar(Exemplo exec)
+        {
+            exec.Executar();
+        }
+    }
+}
